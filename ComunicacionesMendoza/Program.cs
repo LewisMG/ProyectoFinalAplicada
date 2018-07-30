cc9a18a03ab603031570538f127cae0f18b5de3e
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2018-07-30 18:50:56 -0400

    Modificado

--- ComunicacionesMendoza/Program.cs
+++ ComunicacionesMendoza/Program.cs
@@ -15,7 +15,7 @@ namespace ComunicacionesMendoza
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new LOGIN());
         }
     }
 }
